79b1479cf69f3e5db1e858e965722060dd2f9f1f
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-23 11:44:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-//your JS code here. If required.
-let promise= new Promise(function(resolve,reject){
-	resolve("Hello, world!");
-})
-promise.then(function success(result){
-  console.log(result);
-})
+// //your JS code here. If required.
+// let promise= new Promise(function(resolve,reject){
+// 	resolve("Hello, world!");
+// })
+// promise.then(function success(result){
+//   console.log(result);
+// })
 
+function delayedHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
